7671f04931681879ddc8ef351cd2f1e1dafa32f9
Nguyễn Dương <duongnguyen4823@gmail.com>
2025-01-27 00:46:12 +0700

    update: setup mysql

--- health_metrics_collector/health_metrics_collector/settings.py
+++ health_metrics_collector/health_metrics_collector/settings.py
@@ -105,10 +105,15 @@ WSGI_APPLICATION = 'health_metrics_collector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # Password validation
